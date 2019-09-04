15a7122a9e41fb69c94a4add47c9a83c251d7f1d
mafro28 <mafroafro@gmail.com>
2019-09-04 19:25:54 +0100

    First upload of files

---
+++ tipuesearch/tipuesearch_content.js
@@ -0,0 +1,11 @@
+
+var tipuesearch = {"pages": [
+     {"title": "Climate Change", "text": "Computing and Climate Change Climate Change Greenhouse gases Carbon dioxide Methane Water Vapour", "url": "index.html"},
+    
+     {"title": "Negative Impact", "text": "Computers' Negative Impact on Climate Change Manufacture Nitrogen trifluoride NF3", "url": "negativeimpact.html"},
+    
+     {"title": "Positive Impact", "text": "Computers’ positive impact on climate change Using computer models to analyse climate change Low power processors Using ICT to reduce emissions", "url": "positiveimpact.html"},
+    
+     {"title": "Saving Energy", "text": "Saving energy Low power processors Using intelligent software Using virtualisation Green energy", "url": "savingenergy.html"}
+     
+]};
